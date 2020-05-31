4f6d8f85c8cff3760d7039c3ded1a91c022017f8
Wang Ziwei <1123107982@qq.com>
2020-05-31 18:38:23 +0800

    project1a finished.

---
+++ proj1a/ArrayDeque.java
@@ -0,0 +1,125 @@
+public class ArrayDeque<E> implements Deque<E> {
+
+    // size == (capacity + lp - fp - 1) % capacity
+    int size;
+    int capacity;
+    E[] items;
+    // When size == 0 or size == capacity, lp == fp + 1;
+    // fp and lp refer to the next position the new item should be put in.
+    int fp;
+    int lp;
+
+    public ArrayDeque() {
+        size = 0;
+        capacity = 8;
+        fp = 0;
+        lp = 1;
+        items = (E[]) new Object[capacity];
+    }
+
+    public ArrayDeque(ArrayDeque other) {
+        size = other.size;
+        capacity = other.capacity;
+        fp = other.fp;
+        lp = other.lp;
+        items = (E[]) new Object[capacity];
+        for (int i = 0; i < other.size(); i++) {
+            addLast((E) other.get(i));
+        }
+    }
+
+    /** resize method will only be invoked when size == capacity, i.e. fp == lp */
+    private void resize(int expandedCapacity) {
+        E[] expandedList = (E[]) new Object[expandedCapacity];
+        // Copy items after fp till the end of the array to the new list where the first item is put in index 1.
+        System.arraycopy(items, lp, expandedList, 1, capacity - lp);
+        // Copy items before lp till the start of the array to the new list,
+        //     where the first item is put after the last item placed during the last arraycopy operation.
+        System.arraycopy(items, fp, expandedList, 1 + capacity - lp, fp + 1);
+        fp = 0;
+        lp = capacity + 1;
+        capacity = expandedCapacity;
+        items = expandedList;
+    }
+
+    @Override
+    public void addFirst(E item) {
+        if (size == capacity) {
+            resize(2 * capacity);
+        }
+
+        items[fp] = item;
+        // mod operation prodeuce negative result when its dividend is a negative integer
+        // add a divisor to the result to produce a positive remainder.
+        fp = --fp % capacity + capacity;
+        size++;
+    }
+
+    @Override
+    public void addLast(E item) {
+        if (size == capacity) {
+            resize(2 * capacity);
+        }
+
+        items[lp] = item;
+        lp = ++lp % capacity;
+        size++;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public void printDeque() {
+
+    }
+
+    @Override
+    public E removeFirst() {
+        if (size == 0) {
+            System.err.println("Trying to remove the first item from an empty list");
+            return null;
+        }
+        // Note that fp may refer to the last item in a real array.
+        fp = ++fp % capacity;
+        E result = items[fp];
+        items[fp] = null;
+        return result;
+    }
+
+    @Override
+    public E removeLast() {
+        if (size == 0) {
+            System.err.println("Trying to remove the last item from an empty list");
+            return null;
+        }
+        // Mod operation prodeuce negative result when its dividend is a negative integer.
+        // Add a divisor to the result to produce a positive remainder.
+        // Note that lp may refer to the first item in a real array.
+        lp = --lp % capacity + capacity;
+        E result = items[lp];
+        items[lp] = null;
+        return result;
+    }
+
+    @Override
+    public E get(int index) {
+        if (index < 0 || index >= size) {
+            System.err.println("Index out of bounds!");
+            return null;
+        }
+
+        return items[(index + fp + 1) % capacity];
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ proj1a/LinkedListDeque.java
@@ -0,0 +1,182 @@
+import com.sun.org.apache.xpath.internal.objects.XNodeSet;
+
+public class LinkedListDeque<T> {
+
+    private Node<T> cursor;
+
+    private static class Node<E> {
+        private E _item;
+        private Node<E> _prev;
+        private Node<E> _next;
+
+        public Node() {
+            _item = null;
+            _prev = null;
+            _next = null;
+        }
+
+        public Node(E item, Node<E> prev, Node<E> next) {
+            _item = item;
+            _prev = prev;
+            _next = next;
+        }
+    }
+
+    private int _size;
+    private Node<T> sentF;
+    private Node<T> sentL;
+
+    public LinkedListDeque() {
+        _size = 0;
+        sentF = new Node<>();
+//      Avoid the special case when list is null and then a new node is added into the list.
+//      In this case, without a fixed last sentinel, the sentL still references the old sentF node,
+//          and need to be adjusted additionally.
+        sentL = new Node<>(null, sentF, null);
+        sentF._next = sentL;
+    }
+
+    public LinkedListDeque(LinkedListDeque<T> other) {
+        _size = other.size();
+        sentF = new Node<>(other.sentF._item, null, null);
+        Node<T> cursor = sentF;
+        Node<T> otherCursor = other.sentF;
+        for (int i = 0; i <= other.size(); i++) {
+            cursor._next = new Node<>(otherCursor._next._item, cursor, null);
+            cursor = cursor._next;
+            otherCursor = otherCursor._next;
+        }
+    }
+
+    public LinkedListDeque<T> copyDeque(LinkedListDeque<T> other) {
+        _size = other.size();
+        sentF = new Node<>(other.sentF._item, null, null);
+        sentF._next = copyNextNode(sentF, other.sentF._next);
+        return this;
+    }
+
+    /**
+     * This method would deep copy the node specified by the 'next'.
+     *
+     * The method receive the present node, a copying node prior to this node copying the next node,
+     * to link the present node with the currently copying node.
+     *
+     * This method is a helper method for the copyDeque method,
+     *     and will set the Deque object sentF reference and sentL reference to the proper nodes.
+     *
+     * @param present the node prior to the copying node.
+     * @param next the node awaiting to be copied.
+     * @return a new node from an entire new deque with each node copied from the node's original deque.
+     */
+    private Node<T> copyNextNode(Node<T> present, Node<T> next) {
+        if (next == null) {
+            sentL = present;
+            return null;
+        }
+        Node<T> copyNextNode = new Node<>(next._item, present, null);
+        copyNextNode._next = copyNextNode(copyNextNode, next._next);
+        return copyNextNode;
+    }
+
+    /**
+     * A deep copy method for Node.
+     *
+     * The method would not only return a new node for the item included in the node,
+     *     but also generate new nodes for every node it references.
+     *
+     * This method is a helper method for the copyDeque method,
+     *     and will set the Deque object sentF reference and sentL reference to the proper nodes.
+     *
+     * @param node the original node awaiting to be copied
+     * @return a new node from an entire new deque with each node copied from the node's original deque.
+     */
+    private Node<T> copyNode(Node<T> node) {
+        Node<T> cNode = new Node<>(node._item, null, null);
+        if (node._prev != null) {
+            cNode._prev = copyNode(node._prev);
+        } else {
+            sentF = cNode;
+        }
+        if (node._next != null) {
+            cNode._next = copyNode(node._next);
+        } else {
+            sentL = cNode;
+        }
+        return cNode;
+    }
+
+    public void addFirst(T item) {
+        Node<T> newNode = new Node<>(item, sentF, sentF._next);
+        newNode._next._prev = newNode;
+        newNode._prev._next = newNode;
+        _size++;
+    }
+
+    public void addLast(T item) {
+        Node<T> newNode = new Node<>(item, sentL._prev, sentL);
+        newNode._next._prev = newNode;
+        newNode._prev._next = newNode;
+        _size++;
+    }
+
+    public boolean isEmpty() {
+        return size() == 0;
+    }
+
+    public int size() {
+        return _size;
+    }
+
+    public void printDeque() {
+        Node<T> cursor = sentF;
+        for (int i = 0; i < size(); i++) {
+            cursor = cursor._next;
+            System.out.print(String.valueOf(cursor._item) + " ");
+        }
+        System.out.println("");
+    }
+
+    public T removeFirst() {
+        Node<T> first = sentF._next;
+        sentF._next = first._next;
+        first._next._prev = sentF;
+        _size--;
+        return first._item;
+    }
+
+    public T removeLast() {
+        Node<T> last = sentL._prev;
+        sentL._prev = last._prev;
+        last._prev._next = sentL;
+        _size--;
+        return last._item;
+    }
+
+    public T get(int index) {
+        if (index >= size()) {
+            System.err.println("index out of bound!");
+            return null;
+        }
+        Node<T> cursor = sentF;
+        for (int i = 0; i <= index; i++) {
+            cursor = cursor._next;
+        }
+        return cursor._item;
+    }
+
+    public T getRecursive(int index) {
+        if (index >= size()) {
+            System.err.println("index out of bound!");
+            return null;
+        }
+        return getHelper(sentF._next, index);
+    }
+
+    private T getHelper(Node<T> cursor, int index) {
+        if (index == 0) {
+            return cursor._item;
+        }
+        return getHelper(cursor._next, index - 1);
+    }
+}
+
