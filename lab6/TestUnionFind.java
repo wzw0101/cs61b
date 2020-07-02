a63fbbd2b0ac0c10baccaae93c4d467a8cf9fd2e
Wang Ziwei <1123107982@qq.com>
2020-07-02 19:48:58 +0800

    Lab6 finished.

---
+++ lab6/TestUnionFind.java
@@ -0,0 +1,51 @@
+import com.sun.xml.internal.bind.v2.schemagen.xmlschema.Union;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class TestUnionFind {
+    @Test
+    public void testSizeOf() {
+        UnionFind uf = new UnionFind(16);
+        assertEquals(1, uf.sizeOf(2));
+        assertEquals(1, uf.sizeOf(3));
+        uf.union(2, 3);
+        assertEquals(2, uf.sizeOf(2));
+        uf.union(4, 5);
+        uf.union(3, 4);
+        assertEquals(4, uf.sizeOf(4));
+    }
+
+    @Test
+    public void testParent() {
+        UnionFind uf = new UnionFind(8);
+        assertEquals(-1, uf.parent(1));
+        uf.union(1, 2);
+        assertEquals(2, uf.parent(1));
+        uf.union(3, 4);
+        assertEquals(4, uf.parent(3));
+        uf.union(1, 3);
+        assertEquals(4, uf.parent(2));
+    }
+
+    @Test
+    public void testFind() {
+        UnionFind uf = new UnionFind(8);
+        assertEquals(1, uf.find(1));
+        uf.union(1, 2);
+        assertEquals(2, uf.find(1));
+        uf.union(3, 4);
+        uf.union(1, 3);
+        assertEquals(4, uf.find(1));
+    }
+
+    @Test
+    public void testConnected() {
+        UnionFind uf = new UnionFind(8);
+        assertFalse(uf.connected(1, 3));
+        uf.union(1, 2);
+        assertTrue(uf.connected(1, 2));
+        uf.union(3, 4);
+        uf.union(2, 4);
+        assertTrue(uf.connected(1, 3));
+    }
+}
--- lab6/UnionFind.java
+++ lab6/UnionFind.java
@@ -1,35 +1,42 @@
+import com.sun.javaws.exceptions.InvalidArgumentException;
+
 public class UnionFind {
 
     // TODO - Add instance variables?
+    int[] a;
 
     /* Creates a UnionFind data structure holding n vertices. Initially, all
        vertices are in disjoint sets. */
     public UnionFind(int n) {
-        // TODO
+        a = new int[n];
+        for (int i = 0; i < a.length; i++) {
+            a[i] = -1;
+        }
     }
 
     /* Throws an exception if v1 is not a valid index. */
     private void validate(int vertex) {
-        // TODO
+        if (vertex < 0 || vertex >= a.length) {
+            throw new IllegalArgumentException("vertex: " + vertex + " is not a valid index.");
+        }
     }
 
     /* Returns the size of the set v1 belongs to. */
     public int sizeOf(int v1) {
-        // TODO
-        return -1;
+        validate(v1);
+        return Math.abs(a[find(v1)]);
     }
 
     /* Returns the parent of v1. If v1 is the root of a tree, returns the
        negative size of the tree for which v1 is the root. */
     public int parent(int v1) {
-        // TODO
-        return -1;
+        validate(v1);
+        return a[v1];
     }
 
     /* Returns true if nodes v1 and v2 are connected. */
     public boolean connected(int v1, int v2) {
-        // TODO
-        return false;
+        return find(v1) == find(v2);
     }
 
     /* Connects two elements v1 and v2 together. v1 and v2 can be any valid 
@@ -38,14 +45,35 @@ public class UnionFind {
        vertex with itself or vertices that are already connected should not 
        change the sets but may alter the internal structure of the data. */
     public void union(int v1, int v2) {
-        // TODO
+        if (connected(v1, v2)) {
+            return;
+        }
+        int root1 = find(v1);
+        int root2 = find(v2);
+        if (sizeOf(v1) <= sizeOf(v2)) {
+            a[root2] += a[root1];
+            a[root1] = root2;
+        } else {
+            a[root1] += a[root2];
+            a[root2] = root1;
+        }
     }
 
     /* Returns the root of the set V belongs to. Path-compression is employed
        allowing for fast search-time. */
     public int find(int vertex) {
-        // TODO
-        return -1;
+        validate(vertex);
+        int cv = vertex;
+        int pv = parent(cv);
+        while (pv >= 0) {
+            cv = pv;
+            pv = parent(cv);
+        }
+        // Path Compression should not be done when the vertex is the root of the tree
+        if (vertex != cv) {
+            a[vertex] = cv;
+        }
+        return cv;
     }
 
 }
